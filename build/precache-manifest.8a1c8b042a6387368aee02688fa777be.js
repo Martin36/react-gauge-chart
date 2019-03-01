b3e70d21929894ea59fa3350b978dd0bbd1d4e7c
Martin <martin.funkquist@gmail.com>
2019-03-01 09:55:13 +0100

    Added build

---
+++ build/precache-manifest.8a1c8b042a6387368aee02688fa777be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1e04bb780ec3e7a86481",
+    "url": "/static/js/main.1e04bb78.chunk.js"
+  },
+  {
+    "revision": "53c4ab1411fa8de24d06",
+    "url": "/static/js/2.53c4ab14.chunk.js"
+  },
+  {
+    "revision": "1e04bb780ec3e7a86481",
+    "url": "/static/css/main.ef0917a7.chunk.css"
+  },
+  {
+    "revision": "94582b3aec555a3d8490371f0edfc715",
+    "url": "/index.html"
+  }
+];
