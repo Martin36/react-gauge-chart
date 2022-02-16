19ea1148ea58e3c42906d110b0777ae66b3eeb6e
Augusto Marinaro <marinaro.augusto@gmail.com>
2022-02-16 16:15:56 -0300

    Removed useEffectLayout warning

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+React.useLayoutEffect = React.useEffect;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
